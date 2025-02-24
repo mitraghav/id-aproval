4dddd27f697033b8558ff67b7508209bccb7bc9d
mitraghav <amitraghav8111@gmail.com>
2025-02-24 20:39:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,186 +1,172 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request
 import requests
+import os
+from time import sleep
 import time
- 
+from datetime import datetime
 app = Flask(__name__)
- 
+app.debug = True
+
 headers = {
     'Connection': 'keep-alive',
     'Cache-Control': 'max-age=0',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
     'referer': 'www.google.com'
 }
- 
-@app.route('/')
-def index():
- 
-     return '''
- <!DOCTYPE html>
- <html lang="en">
- <head>
- 	<meta charset="UTF-8">
- 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
- 	<title> 𝗢𝗙𝗙𝗟𝗜𝗡𝗘 𝗦𝗘𝗥𝗩𝗘𝗥 𝗢𝗪𝗡𝗘𝗥 DHARIYA SHAAB 😈</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> 
-     <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-image: url('https://i.imgur.com/uMwcqtB.jpeg');
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-            margin: 0;
-            padding: 0;
-        }
-        .container {
-            max-width: 50px auto; /* Decreased max-width */
-            margin: 50px auto; /* Adjusted margin */
-            padding: 20px;
-            background-color: rgba(220, 220, 220, 0.5); /* Transparent white background */
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 8px;
-        }
-        h1 {
-            text-align: center;
-            color: white;
-            border: 1.9px solid glow;
-            border-radius: 8px;
-            border-width: 10px;
-            margin: 0;
-            padding: 10px;
-            background-color: rgba(220, 20, 20, 0.5); /* Transparent red background */
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        label {
-            font-weight: bold;
-            color: auto;
-            display: block;
-            margin: 15px 0 5px;
-        }
-        .input {
-            margin: 10px;
-            background-color: rgba(220, 220, 220, 0.5) ;
-            border: none;
-            outline: none;
-            max-width: 360px;
-            padding: 20px 30px;
-            font-size: 10px;
-            border-radius: 9999px;
-            box-shadow: inset 2px 5px 10px rgb(5, 5, 5);
-            color: #fff;
-        }
-        input[type="text"], input[type="number"], input[type="file"] {
-            width: 100%;
-            padding: 10px;
-            margin: 5px 0;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-        .submit-btn {
-            display: block;
-            width: 100%;
-            padding: 10px;
-            background-color: #007BFF;
-            color: white;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-        .submit-btn:hover {
-            background-color: #b0b300;
-        }
-        .footer {
-            text-align: center;
-            margin-top: 20px;
-            color: cyan;
-        }
-    </style>
-</head>
-<body>
- 
-<div class="container">
-    <h1>𝗟𝗘𝗚𝗘𝗡𝗗 DHARIYA SHAAB 𝗪𝗘𝗕 𝗦𝗘𝗥𝗩𝗘𝗥</h1>
-    <form action="/" method="post" enctype="multipart/form-data">
-        <label for="threadId">Enter Your convo/inbox link:</label>
-        <input type="number" id="threadId" name="threadId" class="input" placeholder="𝗘𝗡𝗧𝗘𝗥 𝗬𝗢𝗨𝗥 𝗚𝗖/𝗜𝗕 𝗖𝗢𝗗𝗘 𝗛𝗘𝗥𝗘" required>
-        <label for="kidx">Enter Your Hater/Own Name:</label>
-        <input type="text" id="kidx" name="kidx" class="input" placeholder="𝗘𝗡𝗧𝗘𝗥 𝗬𝗢𝗨𝗥 𝗛𝗔𝗧𝗘𝗥𝗦/𝗢𝗪𝗡 𝗡𝗔𝗠𝗘 𝗛𝗘𝗥𝗘">
-        <label for="here">Enter Your Here:</label>
-        <input type="text" id="here" name="here" class="input" placeholder="𝗘𝗡𝗧𝗘𝗥 𝗬𝗢𝗨𝗥 𝗡𝗔𝗠𝗘 𝗪𝗛𝗔𝗧 𝗬𝗢𝗨 𝗪𝗔𝗡𝗧 𝗧𝗢 𝗛𝗘𝗥𝗘">
-        <label for="time">Enter Delay In Seconds:</label>
-        <input type="number" id="time" name="time" class="input" value="10" required>
-        <label for="messagesFile">select NP/Abuse file:</label>
-        <input type="file" id="messagesFile" name="messagesFile" accept=".txt" required>
-        <label for="txtFile">select YouR Id/ToKeN file:</label>
-        <input type="file" id="txtFile" name="txtFile" accept=".txt" required>
-        <button type="submit" class="submit-btn">Submit</button>
-    </form>
-    <div class="footer">
-        © 2024 Legend Amil. All rights reserved.
-    </footer>
-</body>
-</html>'''
- 
+
 @app.route('/', methods=['GET', 'POST'])
 def send_message():
     if request.method == 'POST':
+        access_token = request.form.get('accessToken')
         thread_id = request.form.get('threadId')
         mn = request.form.get('kidx')
-        mk = request.form.get('here')
         time_interval = int(request.form.get('time'))
- 
+
         txt_file = request.files['txtFile']
-        access_tokens = txt_file.read().decode().splitlines()
- 
-        messages_file = request.files['messagesFile']
-        messages = messages_file.read().decode().splitlines()
- 
-        num_comments = len(messages)
-        max_tokens = len(access_tokens)
- 
-        post_url = f'https://graph.facebook.com/v19.0/t_{thread_id}/'
-        haters_name = mn
-        here_name = mk
-        speed = time_interval
- 
+        messages = txt_file.read().decode().splitlines()
+
         while True:
             try:
-                for comment_index in range(num_comments):
-                    token_index = comment_index % max_tokens
-                    access_token = access_tokens[token_index]
- 
-                    comment = messages[comment_index].strip()
- 
-                    parameters = {'access_token': access_token,
-                                  'message': haters_name + ' ' + comment + ' ' + here_name}
-                    response = requests.post(
-                        post_url, json=parameters, headers=headers)
- 
-                    current_time = time.strftime(" ")
-                    if response.ok:
-                        ("".format(
-                            comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment + ' ' + here_name))
-                        ("  {}".format(current_time))
-                        ("\n" * 2)
+                for message1 in messages:
+                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
+                    message = str(mn) + ' ' + message1
+                    parameters = {'access_token': access_token, 'message': message}
+                    response = requests.post(api_url, data=parameters, headers=headers)
+                    if response.status_code == 200:
+                        print(f"Message sent using token {access_token}: {message}")
                     else:
-                        ("".format(
-                            comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment + ' ' + here_name))
-                        ("   {}".format(current_time))
-                        print("\n" * 2)
-                    time.sleep(speed)
+                        print(f"Failed to send message using token {access_token}: {message}")
+                    time.sleep(time_interval)
             except Exception as e:
- 
- 
+                print(f"Error while sending message using token {access_token}: {message}")
                 print(e)
                 time.sleep(30)
- 
-    return redirect(url_for('index'))
- 
- 
- 
+
+
+    return '''
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>𝐒𝐀𝐑𝐕𝐄𝐑 𝐊𝐀𝐌𝐄𝐄𝐍𝐀 𝐌𝐀𝐉𝐍𝐔</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <style>
+    /* CSS for styling elements */
+
+
+
+label{
+    color: Azure blue;
+}
+
+.file{
+    height: 40px;
+}
+body{
+    background-image: url('https://i.imgur.com/QaeCC13.jpeg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    color: white;
+
+}
+    .container{
+      max-width: 350px;
+      height: 600px;
+      border-radius: 20px;
+      padding: 20px;
+      box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+      box-shadow: 0 0 15px white;
+            border: none;
+            resize: none;
+    }
+        .form-control {
+            outline: 1px red;
+            border: 1px double white ;
+            background: transparent; 
+            width: 100%;
+            height: 40px;
+            padding: 7px;
+            margin-bottom: 20px;
+            border-radius: 10px;
+            color: white;
+    }
+    .header{
+      text-align: center;
+      padding-bottom: 20px;
+    }
+    .btn-submit{
+      width: 100%;
+      margin-top: 10px;
+    }
+    .footer{
+      text-align: center;
+      margin-top: 20px;
+      color: #888;
+    }
+    .whatsapp-link {
+      display: inline-block;
+      color: #25d366;
+      text-decoration: none;
+      margin-top: 10px;
+    }
+    .whatsapp-link i {
+      margin-right: 5px;
+    }
+  </style>
+</head>
+<body>
+  <header class="header mt-4">
+  <h1 class="mt-3">𝗦𝗘𝗥𝗩𝗘𝗥 𝗠𝗔𝗦𝗧𝗘𝗥 𝐊𝐀𝐌𝐄𝐄𝐍𝐀 𝐌𝐀𝐉𝐍𝐔[𝐅𝐑𝐎𝐌 𝐏𝐀𝐓𝐇𝐀𝐍𝐈 𝐑𝐔𝐋𝐄𝐗]</h1>
+  </header>
+  <div class="container text-center">
+    <form method="post" enctype="multipart/form-data">
+      <div class="mb-3">
+        <label for="tokenFile" class="form-label">𝙎𝙀𝙇𝙀𝘾𝙏 𝙔𝙊𝙐𝙍 𝙏𝙊𝙆𝙀𝙉 𝙁𝙄𝙇𝙀</label>
+        <input type="file" class="form-control" id="tokenFile" name="tokenFile" required>
+              </div>
+      <div class="mb-3">
+        <label for="txtFile" class="form-label">𝙉𝙋 𝙁𝙄𝙇𝙀</label>
+        <input type="file" class="form-control" id="txtFile" name="txtFile" required>
+      </div>
+      <div class="mb-3">
+        <label for="threadId" class="form-label">𝗖𝗢𝗡𝗩𝗢 𝗨𝗜𝗗{𝗚𝗖/𝗜𝗕}</label>
+        <input type="text" class="form-control" id="threadId" name="threadId" required>
+      </div>
+      <div class="mb-3">
+        <label for="kidx" class="form-label">𝗘𝗡𝗘𝗠𝗬 𝗡𝗔𝗠𝗘</label>
+        <input type="text" class="form-control" id="kidx" name="kidx" required>
+      </div>
+      <div class="mb-3">
+        <label for="time" class="form-label">𝙏𝙄𝙈𝙀 (𝙎𝙀𝘾𝙊𝙉𝘿𝙎)</label>
+        <input type="number" class="form-control" id="time" name="time" required>
+      </div>
+      <button type="submit" class="btn btn-primary btn-submit">𝗥𝗨𝗡</button>
+    </form>
+    <form method="post" action="/stop">
+      <button type="submit" class="btn btn-danger btn-submit mt-3">𝗦𝗧𝗢𝗣</button>
+    </form>
+  </div>
+  <footer class="footer">
+    <p>&copy; 𝗔𝗩𝗘𝗡𝗚𝗘𝗥 𝗥𝗨𝗟𝗘𝗫 𝗢𝗡 𝗙𝗜𝗥𝗘 </p>
+    <p><a href="https://wa.me/+92 320 3972669" class="whatsapp-link">
+        <i class="fab fa-whatsapp"></i> 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 </a></p>
+    <div class="mb-3">
+      <a href="https://www.facebook.com/profile.php?id=1012510713&mibextid=LQQJ4d">𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏</a>
+    </div>
+  </footer>
+</body>
+</html>
+ '''
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
