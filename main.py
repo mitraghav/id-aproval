c4d5bb4c2ee2038843f5fd4e7d16c066d30d52a4
mitraghav <amitraghav8111@gmail.com>
2025-07-26 22:26:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def login():
         # Check if the username and password are correct
         if username == 'BROTHER GANG' and password == 'BROTHER1234':
             # Redirect to the specified link if login is successful
-            return redirect('https://pnnal-script-production.up.railway.app/')
+            return redirect('fi11.bot-hosting.net:21156')
         else:
             error = 'Invalid username or password. Please try again.'
 
